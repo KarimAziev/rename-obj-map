5d93bade8ec12008dcc4065fb8bb06ea491f0d1f
KarimAziev <karim.aziiev@gmail.com>
2023-03-20 16:42:44 +0200

    make renameKeys autocurried

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@
  * @param RenameMap - object of form `{oldKey: newKey}`.
  * @param Obj - object to rename. When some key is not found in the renameMap, then it's passed as-is.
  * @returns The generated type
- * @private
  */
 
 export type RenameByMap<RenameMap, Obj> = {
@@ -19,16 +18,12 @@ export type Cast<A, B> = A extends B ? A : B;
 
 /**
  * Types that can be directly narrowed when inferred
- * @private
  */
+
 export type Narrowable = string | number | bigint | boolean;
 
 /**
  * Narrows a generic type that could contain narrowable types
- * @private
- *
- * @type Param A - The type to be narrowed
- * @returns The narrowed type
  */
 export type Narrow<A> = Cast<
   A,
@@ -36,67 +31,66 @@ export type Narrow<A> = Cast<
 >;
 
 /**
+ * Create a function that returns a new object with the own properties of the provided object, but the
+ * keys renamed according to renameMap.
+ * @param renameMap - object of form `{oldKey: newKey}`.
+ * @example
+ * ```ts
+ * const myRename = renameKeys({ _id: 'id' });
+ * myRename({ _id: 1 }) // RESULT: { id: 1 }
+ * ```
+ */
+
+export function renameKeys<RenameMap>(renameMap: Narrow<RenameMap>): <Obj>(
+  obj: Obj,
+) => {
+  [K in keyof Obj as K extends keyof RenameMap
+    ? RenameMap[K] extends string
+      ? RenameMap[K]
+      : never
+    : K]: K extends keyof Obj ? Obj[K] : never;
+};
+/**
  * Creates a new object with the own properties of the provided object, but the
- * keys renamed according to renameMap
+ * keys renamed according to renameMap.
  * @param renameMap - object of form `{oldKey: newKey}`.
  * @param obj - object to rename. When some key is not found in the renameMap, then it's passed as-is.
  * @example
- * renameKeys({ _id: 'id' }, { _id: 1234578 });
- * RESULTS: { id: 1234578 }
- *
- * @example
- * renameKeys({ _id: 'id' }, { _id: 1234578, name: 'John' });
- * RESULTS: { id: 1234578, name: 'John' }
- *
- * @example
- * renameKeys({ _id: 'id', name: 'user' }, { _id: 1234578, name: 'John' });
- * RESULTS: { id: 1234578, user: 'John' }
+ * ```ts
+ * const obj = { _id: 1 };
+ * renameKeys({ _id: 'id' }, obj); // RESULT: { id: 1 }
+ * ```
  */
 
 export function renameKeys<RenameMap, Obj>(
   renameMap: Narrow<RenameMap>,
-  obj: Obj
-) {
-  const keys = Object.keys(obj as any);
-  return keys.reduce(
-    (acc, key) => {
-      const newName = (renameMap as any)[key as keyof RenameMap & Obj] || key;
-      (acc as Record<any, any>)[newName] = (obj as Record<any, any>)[key];
-
-      return acc;
-    },
-    {} as {
-      [K in keyof Obj as K extends keyof RenameMap
-        ? RenameMap[K] extends string
-          ? RenameMap[K]
-          : never
-        : K]: K extends keyof Obj ? Obj[K] : never;
-    }
-  );
-}
-
-export type RenameKeys = typeof renameKeys;
-
+  obj: Obj,
+): {
+  [K in keyof Obj as K extends keyof RenameMap
+    ? RenameMap[K] extends string
+      ? RenameMap[K]
+      : never
+    : K]: K extends keyof Obj ? Obj[K] : never;
+};
 /**
- * Curried version of `renameKeys`  (See {@link renameKeys})
- * @param renameMap - object of form `{ oldKey: newKey }`.
+ * Rename object `obj` according to `renameMap`. This function is automatically curried.
  *
+ * @param renameMap - object of form `{oldKey: newKey}`.
+ * @param obj - object to rename.
  * @example
- * curriedRenameKeys({ _id: 'id' })({ _id: 1234578 });
- * RESULTS: { id: 1234578 }
- *
- * @example
- * curriedRenameKeys({ _id: 'id' })({ _id: 1234578, name: 'John' });
- * RESULTS: { id: 1234578, name: 'John' }
- *
- * @example
- * curriedRenameKeys({ _id: 'id', name: 'user' })({ _id: 1234578, name: 'John' });
- * RESULTS: { id: 1234578, user: 'John' }
+ * ```ts
+ * const renameMap = { _id: 'id' }
+ * const obj = { _id: 1 };
+ * renameKeys(renameMap, obj); // RESULT: { id: 1 }
+ * renameKeys(renameMap)(obj); // RESULT: { id: 1 }
+ * ```
  */
 
-export const curriedRenameKeys =
-  <RenameMap>(renameMap: Narrow<RenameMap>) =>
-  <Obj>(obj: Obj) => {
+export function renameKeys<RenameMap, Obj>(
+  renameMap: Narrow<RenameMap>,
+  obj?: Obj,
+) {
+  if (arguments.length > 1) {
     const keys = Object.keys(obj as any);
     return keys.reduce(
       (acc, key) => {
@@ -111,6 +105,31 @@ export const curriedRenameKeys =
             ? RenameMap[K]
             : never
           : K]: K extends keyof Obj ? Obj[K] : never;
-      }
+      },
     );
-  };
+  } else {
+    return <Obj>(obj: Obj) => {
+      const keys = Object.keys(obj as any);
+      return keys.reduce(
+        (acc, key) => {
+          const newName =
+            (renameMap as any)[key as keyof RenameMap & Obj] || key;
+          (acc as Record<any, any>)[newName] = (obj as Record<any, any>)[key];
+
+          return acc;
+        },
+        {} as {
+          [K in keyof Obj as K extends keyof RenameMap
+            ? RenameMap[K] extends string
+              ? RenameMap[K]
+              : never
+            : K]: K extends keyof Obj ? Obj[K] : never;
+        },
+      );
+    };
+  }
+}
+
+export type RenameKeys = typeof renameKeys;
+
+export const curriedRenameKeys = renameKeys;
